b4c51d0480744ed7130a56b5af64b974a003a095
LinhLTV <lthuc600@gmail.com>
2020-06-26 22:41:00 +0700

    laravel

--- php/06_2020_06_24/ket_noi_database.php
+++ php/06_2020_06_24/ket_noi_database.php
@@ -11,5 +11,16 @@
 			$this->conn = new mysqli($this->host,$this->user,$this->pass,$this->data);
 			$this->conn->query('SET NAMESUFT8');
 		}
+		// xay dung phuong thuc select
+		public function select($sql){
+			$this->connect();
+			$this->result = $this->conn->query($sql);
+		}
+		// xay dung phuong thuc kiem tra va chuyen du lieu
+		public function fetch(){
+			if($this->result->num_rows >0){
+				$row = $this ->result->fetch_assoc();
+			}
+		}
 	}
  ?>
